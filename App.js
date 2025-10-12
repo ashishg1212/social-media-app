4a376763ba04d5254d9297b67fdac02ac3917f0a
Ashish Gunaki <ashishgunaki5@gmail.com>
2025-10-12 21:39:42 +0530

    added fontawesome and done with header part

--- App.js
+++ App.js
@@ -1,13 +1,15 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
-
-import { StatusBar, useColorScheme} from 'react-native';
+import {
+  StatusBar,
+  useColorScheme,
+  View,
+  Text,
+  TouchableOpacity,
+} from 'react-native';
 import Title from './components/Title/Title';
 import { SafeAreaView } from 'react-native-safe-area-context';
+import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome';
+import globalStyle from './assets/styles.js/globalStyle';
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons/faEnvelope';
 
 function App() {
   const isDarkMode = useColorScheme() === 'dark';
@@ -15,7 +17,15 @@ function App() {
   return (
     <SafeAreaView>
       <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <Title title={'Lets Explore'}/>
+      <View style={globalStyle.header}>
+        <Title title={'Let’s Explore'} />
+        <TouchableOpacity style={globalStyle.messageIcon}>
+          <FontAwesomeIcon icon={faEnvelope} size={20} color={'#898DAE'} />
+          <View style={globalStyle.messageNumberContainer}>
+            <Text style={globalStyle.messageNumber}>2</Text>
+          </View>
+        </TouchableOpacity>
+      </View>
     </SafeAreaView>
   );
 }
